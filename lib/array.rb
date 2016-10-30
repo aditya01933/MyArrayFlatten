92a8e1770633d772db4431ac98f4d7f3fac5d2d9
aditya01933 <adityatiwari01933@gmail.com>
2016-10-31 01:38:58 +0530

    handling condition for array of hash and heterogeneous array

--- lib/array.rb
+++ lib/array.rb
@@ -1,7 +1,11 @@
 class Array
   def flat_it
     each_with_object([]) do |element, flattened|
-      flattened.push *(element.is_a?(Array) ? element.flat_it : element)
+      if element.is_a?(Hash) 
+        flattened.push element
+      else
+        flattened.push *(element.is_a?(Array) ? element.flat_it : element)
+      end
     end
   end
 end
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 describe Array do
   let(:flat_array){ [1, 2, 3, 4, 5, 6, 7, 8, 9] }
-  let(:single_nested_array){ [1,2,3,[4,5,6],[7,8], 9] }
-  let(:multiple_nested_array){ [1,2,3,[4,[5,6],7],[8,9]] }
+  let(:single_nested_array){ [ 1, 2, 3, [4, 5, 6], [7, 8], 9] }
+  let(:multiple_nested_array){ [1, 2, 3, [4, [5, 6], 7], [8, 9]] }
+  let(:heterogeneous_array){ [1, [2, 3, "test"], [{car: 5, cities: 10}, 4]]}
+  let(:flat_heterogeneous_array){ [1, 2, 3, "test", {car: 5, cities: 10}, 4]}
 
   it "should perform flatten operation on flat arrays" do
     expect( flat_array.flat_it ).to eq(flat_array)
@@ -21,4 +23,8 @@ describe Array do
     expect( [].flat_it ).to eq([])
   end
 
+  it "should flat heterogeneous array" do
+    expect(heterogeneous_array.flat_it).to eq(flat_heterogeneous_array)
+  end
+  
 end
